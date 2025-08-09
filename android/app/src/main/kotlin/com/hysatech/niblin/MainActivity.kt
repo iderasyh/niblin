8b0cce3142b65543d5dd1088b3fcfe6d24331aab
Redjan Hysa <93912375+iderasyh@users.noreply.github.com>
2025-08-09 16:01:50 +0200

    First push

---
+++ android/app/src/main/kotlin/com/hysatech/niblin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hysatech.niblin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/src/core/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFFF9B73);
+  static const Color secondary = Color(0xFFFFD88A);
+  static const Color tertiary = Color(0xFF9FD6AE);
+  static const Color neutralLight = Color(0xFFFFF9F6);
+  static const Color neutralDark = Color(0xFF3A3A3A);
+  static const Color error = Color(0xFFFF6B6B);
+}
